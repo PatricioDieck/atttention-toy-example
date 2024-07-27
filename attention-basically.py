61b1c054e0f6583f28b44d0d5b187b0ecbc4d68d
Patricio Dieck <123036273+PatricioDieck@users.noreply.github.com>
2024-07-27 11:43:04 -0500

    exploring the nodes

--- attention-basically.py
+++ attention-basically.py
@@ -19,16 +19,37 @@ class Node:
 
     def query(self):
         # what am i looking for?
-        return self.query @ self.data
+        return self.wquery @ self.data
+    # explain what is happening when we do @ here 
+    # we are doing a dot product of the weight matrix and the data vector
 
     def value(self):
         # what do i publicly reveal/broadcast to others?
-        return self.value @ self.data
+        return self.wvalue @ self.data
+
+class Graph :
+
+    def __init__(self):
+        # make 10 nodes
+        self.nodes = [Node() for _ in range(10)]
+        # make 40 edges 
+        randi = lambda: np.random.randint(len(self.nodes))
+        self.edges = [[randi(), randi()] for _ in range(40)]
+
+
+
+
+
+
+
+
+
 
 
 n = Node()
 
 print("DATA of node\n", n.data)
+print("WKEY of node\n", n.wkey)
 
 print("KEYS node\n", n.key)
 
